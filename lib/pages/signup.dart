fa5f22cb8bdc8daef1e79333ab4c43b145aa980a
needrima <oyebodeamirdeen@gmail.com>
2024-05-09 12:21:23 +0100

    commit

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -9,212 +9,243 @@ class Signup extends StatelessWidget {
       body: SafeArea(
           child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            RichText(
-              text: TextSpan(
-                text: 'Create your ',
-                style: Theme.of(context).textTheme.titleLarge,
-                children: <TextSpan>[
-                  TextSpan(
-                      text: 'account',
-                      style: TextStyle(
-                          color: Theme.of(context).colorScheme.inversePrimary)),
-                ],
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 20,
               ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Text(
-              'Lorem ipsum dolor sit amet, consecturadipiscing',
-              style: Theme.of(context).textTheme.displayMedium,
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Form(
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Firstname',
-                      hintStyle: Theme.of(context).textTheme.labelMedium,
-                      prefixIcon: const Icon(
-                        Icons.person,
-                      ),
-                      prefixIconColor:
-                          Theme.of(context).colorScheme.inversePrimary,
-                      border: const OutlineInputBorder(
-                        borderSide: BorderSide(style: BorderStyle.none),
-                      ),
-                      enabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(style: BorderStyle.none),
-                      ),
-                      filled: true,
-                      fillColor: const Color.fromRGBO(34, 34, 34, 0.05),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Lastname',
-                      hintStyle: Theme.of(context).textTheme.labelMedium,
-                      prefixIcon: const Icon(
-                        Icons.person,
-                      ),
-                      prefixIconColor:
-                          Theme.of(context).colorScheme.inversePrimary,
-                      border: const OutlineInputBorder(
-                        borderSide: BorderSide(style: BorderStyle.none),
-                      ),
-                      enabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(style: BorderStyle.none),
-                      ),
-                      filled: true,
-                      fillColor: const Color.fromRGBO(34, 34, 34, 0.05),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Username',
-                      hintStyle: Theme.of(context).textTheme.labelMedium,
-                      prefixIcon: const Icon(
-                        Icons.person,
-                      ),
-                      prefixIconColor:
-                          Theme.of(context).colorScheme.inversePrimary,
-                      border: const OutlineInputBorder(
-                        borderSide: BorderSide(style: BorderStyle.none),
-                      ),
-                      enabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(style: BorderStyle.none),
-                      ),
-                      filled: true,
-                      fillColor: const Color.fromRGBO(34, 34, 34, 0.05),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Email address',
-                      hintStyle: Theme.of(context).textTheme.labelMedium,
-                      prefixIcon: const Icon(
-                        Icons.email,
-                      ),
-                      prefixIconColor:
-                          Theme.of(context).colorScheme.inversePrimary,
-                      border: const OutlineInputBorder(
-                        borderSide: BorderSide(style: BorderStyle.none),
-                      ),
-                      enabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(style: BorderStyle.none),
-                      ),
-                      filled: true,
-                      fillColor: const Color.fromRGBO(34, 34, 34, 0.05),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Phone number',
-                      hintStyle: Theme.of(context).textTheme.labelMedium,
-                      prefixIcon: const Icon(
-                        Icons.phone_rounded,
-                      ),
-                      prefixIconColor:
-                          Theme.of(context).colorScheme.inversePrimary,
-                      border: const OutlineInputBorder(
-                        borderSide: BorderSide(style: BorderStyle.none),
-                      ),
-                      enabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(style: BorderStyle.none),
-                      ),
-                      filled: true,
-                      fillColor: const Color.fromRGBO(34, 34, 34, 0.05),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      hintStyle: Theme.of(context).textTheme.labelMedium,
-                      prefixIcon: const Icon(
-                        Icons.lock,
-                      ),
-                      prefixIconColor:
-                          Theme.of(context).colorScheme.inversePrimary,
-                      border: const OutlineInputBorder(
-                        borderSide: BorderSide(style: BorderStyle.none),
-                      ),
-                      enabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(style: BorderStyle.none),
-                      ),
-                      filled: true,
-                      fillColor: const Color.fromRGBO(34, 34, 34, 0.05),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Confirm password',
-                      hintStyle: Theme.of(context).textTheme.labelMedium,
-                      prefixIcon: const Icon(
-                        Icons.lock,
-                      ),
-                      prefixIconColor:
-                          Theme.of(context).colorScheme.inversePrimary,
-                      border: const OutlineInputBorder(
-                        borderSide: BorderSide(style: BorderStyle.none),
-                      ),
-                      enabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(style: BorderStyle.none),
-                      ),
-                      filled: true,
-                      fillColor: const Color.fromRGBO(34, 34, 34, 0.05),
-                    ),
-                  ),
-                  // const SizedBox(
-                  //   height: 8,
-                  // ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      GestureDetector(
-                        child: const Text('Forgot password?'),
-                        onTap: () {
-                          print('forgot password clicked');
-                        },
-                      ),
-                      GestureDetector(
-                        child: const Text('Show password'),
-                        onTap: () {
-                          print('show password clicked');
-                        },
-                      ),
-                    ],
-                  )
-                ],
+              RichText(
+                text: TextSpan(
+                  text: 'Create your ',
+                  style: Theme.of(context).textTheme.titleLarge,
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'account',
+                        style: TextStyle(
+                            color:
+                                Theme.of(context).colorScheme.inversePrimary)),
+                  ],
+                ),
               ),
-            )
-          ],
+              const SizedBox(
+                height: 15,
+              ),
+              Text(
+                'Lorem ipsum dolor sit amet, consecturadipiscing',
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Form(
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Firstname',
+                        hintStyle: Theme.of(context).textTheme.labelMedium,
+                        prefixIcon: const Icon(
+                          Icons.person,
+                        ),
+                        prefixIconColor:
+                            Theme.of(context).colorScheme.inversePrimary,
+                        border: const OutlineInputBorder(
+                          borderSide: BorderSide(style: BorderStyle.none),
+                        ),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(style: BorderStyle.none),
+                        ),
+                        filled: true,
+                        fillColor: const Color.fromRGBO(34, 34, 34, 0.05),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Lastname',
+                        hintStyle: Theme.of(context).textTheme.labelMedium,
+                        prefixIcon: const Icon(
+                          Icons.person,
+                        ),
+                        prefixIconColor:
+                            Theme.of(context).colorScheme.inversePrimary,
+                        border: const OutlineInputBorder(
+                          borderSide: BorderSide(style: BorderStyle.none),
+                        ),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(style: BorderStyle.none),
+                        ),
+                        filled: true,
+                        fillColor: const Color.fromRGBO(34, 34, 34, 0.05),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Username',
+                        hintStyle: Theme.of(context).textTheme.labelMedium,
+                        prefixIcon: const Icon(
+                          Icons.person,
+                        ),
+                        prefixIconColor:
+                            Theme.of(context).colorScheme.inversePrimary,
+                        border: const OutlineInputBorder(
+                          borderSide: BorderSide(style: BorderStyle.none),
+                        ),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(style: BorderStyle.none),
+                        ),
+                        filled: true,
+                        fillColor: const Color.fromRGBO(34, 34, 34, 0.05),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Email address',
+                        hintStyle: Theme.of(context).textTheme.labelMedium,
+                        prefixIcon: const Icon(
+                          Icons.email,
+                        ),
+                        prefixIconColor:
+                            Theme.of(context).colorScheme.inversePrimary,
+                        border: const OutlineInputBorder(
+                          borderSide: BorderSide(style: BorderStyle.none),
+                        ),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(style: BorderStyle.none),
+                        ),
+                        filled: true,
+                        fillColor: const Color.fromRGBO(34, 34, 34, 0.05),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Phone number',
+                        hintStyle: Theme.of(context).textTheme.labelMedium,
+                        prefixIcon: const Icon(
+                          Icons.phone_rounded,
+                        ),
+                        prefixIconColor:
+                            Theme.of(context).colorScheme.inversePrimary,
+                        border: const OutlineInputBorder(
+                          borderSide: BorderSide(style: BorderStyle.none),
+                        ),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(style: BorderStyle.none),
+                        ),
+                        filled: true,
+                        fillColor: const Color.fromRGBO(34, 34, 34, 0.05),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        hintStyle: Theme.of(context).textTheme.labelMedium,
+                        prefixIcon: const Icon(
+                          Icons.lock,
+                        ),
+                        prefixIconColor:
+                            Theme.of(context).colorScheme.inversePrimary,
+                        border: const OutlineInputBorder(
+                          borderSide: BorderSide(style: BorderStyle.none),
+                        ),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(style: BorderStyle.none),
+                        ),
+                        filled: true,
+                        fillColor: const Color.fromRGBO(34, 34, 34, 0.05),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Confirm password',
+                        hintStyle: Theme.of(context).textTheme.labelMedium,
+                        prefixIcon: const Icon(
+                          Icons.lock,
+                        ),
+                        prefixIconColor:
+                            Theme.of(context).colorScheme.inversePrimary,
+                        border: const OutlineInputBorder(
+                          borderSide: BorderSide(style: BorderStyle.none),
+                        ),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(style: BorderStyle.none),
+                        ),
+                        filled: true,
+                        fillColor: const Color.fromRGBO(34, 34, 34, 0.05),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        GestureDetector(
+                          child: const Text('Forgot password?'),
+                          onTap: () {
+                            print('forgot password clicked');
+                          },
+                        ),
+                        GestureDetector(
+                          child: const Text('Show password'),
+                          onTap: () {
+                            print('show password clicked');
+                          },
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        print('about to signup');
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll(
+                            Theme.of(context).colorScheme.inversePrimary),
+                        shape: const MaterialStatePropertyAll(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8),
+                            ),
+                          ),
+                        ),
+                      ),
+                      child: const Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 45, vertical: 10),
+                        child: Text(
+                          'Create Account',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       )),
     );
