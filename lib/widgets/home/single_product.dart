85db4e3e91416ba8a113d6e59260461eecd712b8
needrima <oyebodeamirdeen@gmail.com>
2024-06-02 22:16:10 +0100

    bottom modal for inbox

--- lib/widgets/home/single_product.dart
+++ lib/widgets/home/single_product.dart
@@ -10,6 +10,7 @@ class SingleProduct extends StatefulWidget {
 
 class _SingleProductState extends State<SingleProduct> {
   final PageController pageController = PageController();
+  final TextEditingController _commentInputController = TextEditingController();
 
   @override
   void initState() {
@@ -67,7 +68,7 @@ class _SingleProductState extends State<SingleProduct> {
                 ),
                 GestureDetector(
                   onTap: () {
-                    print('liked');
+                    print('liking');
                   },
                   child: const Icon(
                     Icons.favorite,
@@ -95,118 +96,204 @@ class _SingleProductState extends State<SingleProduct> {
                         return Container(
                           height: MediaQuery.of(context).size.height * 0.8,
                           padding: const EdgeInsets.symmetric(horizontal: 10),
-                          child: ListView.separated(
-                            itemBuilder: (context, index) {
-                              return ListTile(
-                                subtitle: Column(
+                          child: Stack(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 120),
+                                child: ListView.separated(
+                                  itemBuilder: (context, index) {
+                                    return ListTile(
+                                      subtitle: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          const Text(
+                                            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, nostrum eius maiores labore vitae quibusdam recusandae molestias cupiditate in consequatur? A repellat iste sequi, odit neque asperiores quam, impedit debitis obcaecati totam tenetur laudantium? Dolores corporis cumque minima vero fugit.',
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                          const SizedBox(
+                                            height: 6,
+                                          ),
+                                          Row(
+                                            children: [
+                                              Text(
+                                                '02-01-2024 6:30pm',
+                                                style: TextStyle(
+                                                  color: Theme.of(context)
+                                                      .colorScheme
+                                                      .inversePrimary,
+                                                  fontSize: 10,
+                                                  fontWeight: FontWeight.w700,
+                                                ),
+                                              ),
+                                              const SizedBox(
+                                                width: 15,
+                                              ),
+                                              const Column(
+                                                children: [
+                                                  Icon(
+                                                    Icons.favorite,
+                                                    color: Colors.white,
+                                                    size: 18,
+                                                  ),
+                                                  Text(
+                                                    '200',
+                                                    style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontSize: 10,
+                                                      fontWeight:
+                                                          FontWeight.w700,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                              const SizedBox(
+                                                width: 15,
+                                              ),
+                                              const Column(
+                                                children: [
+                                                  Icon(
+                                                    Icons.reply_rounded,
+                                                    color: Colors.white,
+                                                    size: 20,
+                                                  ),
+                                                  Text(
+                                                    '12',
+                                                    style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontSize: 10,
+                                                      fontWeight:
+                                                          FontWeight.w700,
+                                                    ),
+                                                  ),
+                                                ],
+                                              )
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                      trailing: PopupMenuButton(
+                                        icon: const Icon(Icons.more_vert),
+                                        onSelected: (result) {
+                                          // Handle the menu selection
+                                          print('Selected: $result');
+                                        },
+                                        itemBuilder: (BuildContext context) => [
+                                          const PopupMenuItem(
+                                            value: 'edit',
+                                            child: Text(
+                                              'Edit',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.w700,
+                                              ),
+                                            ),
+                                          ),
+                                          const PopupMenuItem(
+                                            value: 'delete',
+                                            child: Text(
+                                              'Delete',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.w700,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      // tileColor: Colors.transparent,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                    );
+                                  },
+                                  separatorBuilder: (context, index) {
+                                    return const SizedBox(
+                                      height: 10,
+                                    );
+                                  },
+                                  itemCount: 5,
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 0,
+                                child: Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    const Text(
-                                      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, nostrum eius maiores labore vitae quibusdam recusandae molestias cupiditate in consequatur? A repellat iste sequi, odit neque asperiores quam, impedit debitis obcaecati totam tenetur laudantium? Dolores corporis cumque minima vero fugit.',
-                                      style: TextStyle(color: Colors.white),
+                                    SizedBox(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.9,
+                                      child: TextField(
+                                        maxLines: null,
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 12,
+                                        ),
+                                        controller: _commentInputController,
+                                        decoration: const InputDecoration(
+                                            hintText: 'Leave a comment',
+                                            hintStyle: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.w400,
+                                              fontSize: 12,
+                                            ),
+                                            border: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                style: BorderStyle.solid,
+                                                color: Colors.white,
+                                              ),
+                                              borderRadius: BorderRadius.all(
+                                                Radius.circular(10),
+                                              ),
+                                            ),
+                                            enabledBorder: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                style: BorderStyle.solid,
+                                                color: Colors.white,
+                                              ),
+                                              borderRadius: BorderRadius.all(
+                                                Radius.circular(10),
+                                              ),
+                                            ),
+                                            filled: true,
+                                            fillColor: Colors.black),
+                                      ),
                                     ),
                                     const SizedBox(
-                                      height: 6,
+                                      height: 8,
                                     ),
-                                    Row(
-                                      children: [
-                                        Text(
-                                          '02-01-2024 6:30pm',
-                                          style: TextStyle(
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .inversePrimary,
-                                            fontSize: 10,
-                                            fontWeight: FontWeight.w700,
+                                    TextButton(
+                                      onPressed: () {
+                                        print('commenting');
+                                      },
+                                      style: ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStatePropertyAll(
+                                          Theme.of(context)
+                                              .colorScheme
+                                              .inversePrimary,
+                                        ),
+                                        shape: const MaterialStatePropertyAll(
+                                          RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.all(
+                                              Radius.circular(8),
+                                            ),
                                           ),
                                         ),
-                                        const SizedBox(
-                                          width: 15,
-                                        ),
-                                        Column(
-                                          children: [
-                                            const Icon(
-                                              Icons.favorite,
-                                              color: Colors.white,
-                                              size: 18,
-                                            ),
-                                            Text(
-                                              '(200)',
-                                              style: TextStyle(
-                                                color: Theme.of(context)
-                                                    .colorScheme
-                                                    .inversePrimary,
-                                                fontSize: 10,
-                                                fontWeight: FontWeight.w700,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                        const SizedBox(
-                                          width: 15,
-                                        ),
-                                        Column(
-                                          children: [
-                                            const Icon(
-                                              Icons.reply_rounded,
-                                              color: Colors.white,
-                                              size: 20,
-                                            ),
-                                            Text(
-                                              '(12)',
-                                              style: TextStyle(
-                                                color: Theme.of(context)
-                                                    .colorScheme
-                                                    .inversePrimary,
-                                                fontSize: 10,
-                                                fontWeight: FontWeight.w700,
-                                              ),
-                                            ),
-                                          ],
-                                        )
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                                trailing: PopupMenuButton(
-                                  icon: const Icon(Icons.more_vert),
-                                  onSelected: (result) {
-                                    // Handle the menu selection
-                                    print('Selected: $result');
-                                  },
-                                  itemBuilder: (BuildContext context) => [
-                                    const PopupMenuItem(
-                                      value: 'edit',
-                                      child: Text(
-                                        'Edit',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.w700,
-                                        ),
                                       ),
-                                    ),
-                                    const PopupMenuItem(
-                                      value: 'delete',
-                                      child: Text(
-                                        'Delete',
+                                      child: const Text(
+                                        'Comment',
                                         style: TextStyle(
+                                          color: Colors.white,
                                           fontWeight: FontWeight.w700,
+                                          fontSize: 10,
                                         ),
                                       ),
                                     ),
                                   ],
                                 ),
-                                // tileColor: Colors.transparent,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(5),
-                                ),
-                              );
-                            },
-                            separatorBuilder: (context, index) {
-                              return const SizedBox(
-                                height: 10,
-                              );
-                            },
-                            itemCount: 10,
+                              ),
+                            ],
                           ),
                         );
                       },
